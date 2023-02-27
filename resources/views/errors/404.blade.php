bdf00f212033924b7ee9b4cb62ce95e12c342cbe
magiksen <elemejore@gmail.com>
2023-02-26 21:53:30 -0400

    Pagina 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,7 +2,7 @@
 @section('main')
 
 @section('title')
-   404 Page
+   Página 404
 @endsection
 
 
@@ -11,15 +11,15 @@
                 <div class="row">
                     <div class="col-xl-8 col-lg-10 col-md-12 m-auto text-center">
                         <p class="mb-20"><img src="{{ asset('frontend/assets/imgs/page/page-404.png') }}" alt="" class="hover-up" /></p>
-                        <h1 class="display-2 mb-30">Page Not Found</h1>
+                        <h1 class="display-2 mb-30">Página no encontrada</h1>
                         <p class="font-lg text-grey-700 mb-30">
-                            The link you clicked may be broken or the page may have been removed.<br />
-                            visit the <a href="#"> <span> Homepage</span></a> or <a href="page-contact.html"><span>Contact us</span></a> about the problem
+                            El enlace que ha pulsado puede estar roto o la página puede haber sido eliminada.<br />
+                            visita la <a href="{{ route('homepage') }}"> <span> página de inicio</span></a> o <a href="page-contact.html"><span>contactanos</span></a> sobre el problema
                         </p>
                         <div class="search-form">
 
                         </div>
-                        <a class="btn btn-default submit-auto-width font-xs hover-up mt-30" href="{{ url('/') }}"><i class="fi-rs-home mr-5"></i> Back To Home Page</a>
+                        <a class="btn btn-default submit-auto-width font-xs hover-up mt-30" href="{{ route('homepage') }}"><i class="fi-rs-home mr-5"></i> Regresar a la página de inicio</a>
                     </div>
                 </div>
             </div>
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,11 +2,11 @@
 @section('main')
 
 @section('title')
-    Home Easy Multi Vendor Shop 
+    Home Easy Multi Vendor Shop
 @endsection
 
      @include('frontend.home.home_slider')
- 
+
         <!--End hero slider-->
      @include('frontend.home.home_features_category')
 
@@ -18,14 +18,14 @@
 	@include('frontend.home.home_new_product')
 
         <!--Products Tabs-->
- 
- 
+
+
 
     @include('frontend.home.home_features_product')
 
-        
+
         <!--End Best Sales-->
- 
+
 
 
 
@@ -34,8 +34,8 @@
     <section class="product-tabs section-padding position-relative">
             <div class="container">
                 <div class="section-title style-2 wow animate__animated animate__fadeIn">
-                    <h3>{{ $skip_category_0->category_name }} Category </h3>
-                   
+                    <h3>{{ $skip_category_0->category_name }} Categoría </h3>
+
                 </div>
                 <!--End nav-tabs-->
                 <div class="tab-content" id="myTabContent">
@@ -50,12 +50,12 @@
                 <div class="product-img product-img-zoom">
                     <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}">
                         <img class="default-img" src="{{ asset( $product->product_thambnail ) }}" alt="" />
-                        
+
                     </a>
                 </div>
                 <div class="product-action-1">
    <a aria-label="Add To Wishlist" class="action-btn" id="{{ $product->id }}" onclick="addToWishList(this.id)"  ><i class="fi-rs-heart"></i></a>
-                    
+
    <a aria-label="Compare" class="action-btn"  id="{{ $product->id }}" onclick="addToCompare(this.id)"><i class="fi-rs-shuffle"></i></a>
 
    <a aria-label="Quick view" class="action-btn" data-bs-toggle="modal" data-bs-target="#quickViewModal" id="{{ $product->id }}" onclick="productView(this.id)" ><i class="fi-rs-eye"></i></a>
@@ -71,12 +71,12 @@
                 <div class="product-badges product-badges-position product-badges-mrg">
 
                     @if($product->discount_price == NULL)
-                    <span class="new">New</span>
+                    <span class="new">Nuevo</span>
                     @else
                     <span class="hot"> {{ round($discount) }} %</span>
                     @endif
 
-                    
+
                 </div>
             </div>
             <div class="product-content-wrap">
@@ -89,21 +89,21 @@
 $reviewcount = App\Models\Review::where('product_id',$product->id)->where('status',1)->latest()->get();
 
 $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1)->avg('rating');
-@endphp        
+@endphp
 
                 <div class="product-rate-cover">
                     <div class="product-rate d-inline-block">
                        @if($avarage == 0)
-      
-       @elseif($avarage == 1 || $avarage < 2)                     
+
+       @elseif($avarage == 1 || $avarage < 2)
     <div class="product-rating" style="width: 20%"></div>
-       @elseif($avarage == 2 || $avarage < 3)                     
+       @elseif($avarage == 2 || $avarage < 3)
     <div class="product-rating" style="width: 40%"></div>
-       @elseif($avarage == 3 || $avarage < 4)                     
+       @elseif($avarage == 3 || $avarage < 4)
     <div class="product-rating" style="width: 60%"></div>
-       @elseif($avarage == 4 || $avarage < 5)                     
+       @elseif($avarage == 4 || $avarage < 5)
     <div class="product-rating" style="width: 80%"></div>
-       @elseif($avarage == 5 || $avarage < 5)                     
+       @elseif($avarage == 5 || $avarage < 5)
     <div class="product-rating" style="width: 100%"></div>
     @endif
                     </div>
@@ -111,7 +111,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
                 </div>
 
 
-                
+
                 <div>
                     @if($product->vendor_id == NULL)
 <span class="font-small text-muted">By <a href="vendor-details-1.html">Owner</a></span>
@@ -119,7 +119,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
   <span class="font-small text-muted">By <a href="vendor-details-1.html">{{ $product['vendor']['name'] }}</a></span>
 
                     @endif
-                   
+
 
 
                 </div>
@@ -128,7 +128,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
                     @if($product->discount_price == NULL)
                      <div class="product-price">
                         <span>${{ $product->selling_price }}</span>
-                       
+
                     </div>
 
                     @else
@@ -139,26 +139,26 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
                     @endif
 
 
-                     
+
                     <div class="add-cart">
-                        <a class="add" href="shop-cart.html"><i class="fi-rs-shopping-cart mr-5"></i>Add </a>
+                        <a class="add" href="shop-cart.html"><i class="fi-rs-shopping-cart mr-5"></i>Agregar </a>
                     </div>
                 </div>
             </div>
         </div>
-    </div> 
+    </div>
     <!--end product card-->
     @endforeach
 
 
 
-                           
-                           
+
+
                         </div>
                         <!--End product-grid-4-->
                     </div>
-                   
-                   
+
+
                 </div>
                 <!--End tab-content-->
             </div>
@@ -176,8 +176,8 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
    <section class="product-tabs section-padding position-relative">
             <div class="container">
                 <div class="section-title style-2 wow animate__animated animate__fadeIn">
-                    <h3>{{ $skip_category_2->category_name }} Category </h3>
-                   
+                    <h3>{{ $skip_category_2->category_name }} Categoría </h3>
+
                 </div>
                 <!--End nav-tabs-->
                 <div class="tab-content" id="myTabContent">
@@ -192,12 +192,12 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
                 <div class="product-img product-img-zoom">
                     <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}">
                         <img class="default-img" src="{{ asset( $product->product_thambnail ) }}" alt="" />
-                        
+
                     </a>
                 </div>
                 <div class="product-action-1">
                      <a aria-label="Add To Wishlist" class="action-btn" id="{{ $product->id }}" onclick="addToWishList(this.id)"  ><i class="fi-rs-heart"></i></a>
-                    
+
    <a aria-label="Compare" class="action-btn"  id="{{ $product->id }}" onclick="addToCompare(this.id)"><i class="fi-rs-shuffle"></i></a>
 
    <a aria-label="Quick view" class="action-btn" data-bs-toggle="modal" data-bs-target="#quickViewModal" id="{{ $product->id }}" onclick="productView(this.id)" ><i class="fi-rs-eye"></i></a>
@@ -213,12 +213,12 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
                 <div class="product-badges product-badges-position product-badges-mrg">
 
                     @if($product->discount_price == NULL)
-                    <span class="new">New</span>
+                    <span class="new">Nuevo</span>
                     @else
                     <span class="hot"> {{ round($discount) }} %</span>
                     @endif
 
-                    
+
                 </div>
             </div>
             <div class="product-content-wrap">
@@ -239,7 +239,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
   <span class="font-small text-muted">By <a href="vendor-details-1.html">{{ $product['vendor']['name'] }}</a></span>
 
                     @endif
-                   
+
 
 
                 </div>
@@ -248,7 +248,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
                     @if($product->discount_price == NULL)
                      <div class="product-price">
                         <span>${{ $product->selling_price }}</span>
-                       
+
                     </div>
 
                     @else
@@ -259,26 +259,26 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
                     @endif
 
 
-                     
+
                     <div class="add-cart">
-                        <a class="add" href="shop-cart.html"><i class="fi-rs-shopping-cart mr-5"></i>Add </a>
+                        <a class="add" href="shop-cart.html"><i class="fi-rs-shopping-cart mr-5"></i>Agregar </a>
                     </div>
                 </div>
             </div>
         </div>
-    </div> 
+    </div>
     <!--end product card-->
     @endforeach
 
 
 
-                           
-                           
+
+
                         </div>
                         <!--End product-grid-4-->
                     </div>
-                   
-                   
+
+
                 </div>
                 <!--End tab-content-->
             </div>
@@ -288,20 +288,20 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
         <!--End SweetHome Category -->
 
 
- 
 
 
 
 
 
-  
+
+
         <!-- Mobile Category -->
 
    <section class="product-tabs section-padding position-relative">
             <div class="container">
                 <div class="section-title style-2 wow animate__animated animate__fadeIn">
                     <h3>{{ $skip_category_7->category_name }} Category </h3>
-                   
+
                 </div>
                 <!--End nav-tabs-->
                 <div class="tab-content" id="myTabContent">
@@ -316,12 +316,12 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
                 <div class="product-img product-img-zoom">
                     <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}">
                         <img class="default-img" src="{{ asset( $product->product_thambnail ) }}" alt="" />
-                        
+
                     </a>
                 </div>
                 <div class="product-action-1">
                     <a aria-label="Add To Wishlist" class="action-btn" id="{{ $product->id }}" onclick="addToWishList(this.id)"  ><i class="fi-rs-heart"></i></a>
-                    
+
    <a aria-label="Compare" class="action-btn"  id="{{ $product->id }}" onclick="addToCompare(this.id)"><i class="fi-rs-shuffle"></i></a>
 
    <a aria-label="Quick view" class="action-btn" data-bs-toggle="modal" data-bs-target="#quickViewModal" id="{{ $product->id }}" onclick="productView(this.id)" ><i class="fi-rs-eye"></i></a>
@@ -342,7 +342,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
                     <span class="hot"> {{ round($discount) }} %</span>
                     @endif
 
-                    
+
                 </div>
             </div>
             <div class="product-content-wrap">
@@ -363,7 +363,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
   <span class="font-small text-muted">By <a href="vendor-details-1.html">{{ $product['vendor']['name'] }}</a></span>
 
                     @endif
-                   
+
 
 
                 </div>
@@ -372,7 +372,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
                     @if($product->discount_price == NULL)
                      <div class="product-price">
                         <span>${{ $product->selling_price }}</span>
-                       
+
                     </div>
 
                     @else
@@ -383,26 +383,26 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
                     @endif
 
 
-                     
+
                     <div class="add-cart">
                         <a class="add" href="shop-cart.html"><i class="fi-rs-shopping-cart mr-5"></i>Add </a>
                     </div>
                 </div>
             </div>
         </div>
-    </div> 
+    </div>
     <!--end product card-->
     @endforeach
 
 
 
-                           
-                           
+
+
                         </div>
                         <!--End product-grid-4-->
                     </div>
-                   
-                   
+
+
                 </div>
                 <!--End tab-content-->
             </div>
@@ -412,7 +412,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
         <!--End Mobile Category -->
 
 
- 
+
 
 
 
@@ -422,8 +422,8 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
                     <div class="col-xl-3 col-lg-4 col-md-6 mb-sm-5 mb-md-0 wow animate__animated animate__fadeInUp" data-wow-delay="0">
                         <h4 class="section-title style-1 mb-30 animated animated"> Hot Deals </h4>
                         <div class="product-list-small animated animated">
-                            
-         @foreach($hot_deals as $item)                   
+
+         @foreach($hot_deals as $item)
     <article class="row align-items-center hover-up">
         <figure class="col-md-4 mb-0">
             <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}"><img src="{{ asset( $item->product_thambnail ) }}" alt="" /></a>
@@ -441,7 +441,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
              @if($item->discount_price == NULL)
                      <div class="product-price">
                         <span>${{ $item->selling_price }}</span>
-                       
+
                     </div>
 
                     @else
@@ -455,7 +455,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
     @endforeach
 
 
-                            
+
                         </div>
                     </div>
 
@@ -465,9 +465,9 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
                     <div class="col-xl-3 col-lg-4 col-md-6 mb-md-0 wow animate__animated animate__fadeInUp" data-wow-delay=".1s">
                         <h4 class="section-title style-1 mb-30 animated animated">  Special Offer </h4>
                         <div class="product-list-small animated animated">
-                            
 
-      @foreach($special_offer as $item)                   
+
+      @foreach($special_offer as $item)
     <article class="row align-items-center hover-up">
         <figure class="col-md-4 mb-0">
             <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}"><img src="{{ asset( $item->product_thambnail ) }}" alt="" /></a>
@@ -485,7 +485,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
              @if($item->discount_price == NULL)
                      <div class="product-price">
                         <span>${{ $item->selling_price }}</span>
-                       
+
                     </div>
 
                     @else
@@ -499,15 +499,15 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
     @endforeach
 
 
-                            
+
                         </div>
                     </div>
                     <div class="col-xl-3 col-lg-4 col-md-6 mb-sm-5 mb-md-0 d-none d-lg-block wow animate__animated animate__fadeInUp" data-wow-delay=".2s">
                         <h4 class="section-title style-1 mb-30 animated animated">Recently added</h4>
                         <div class="product-list-small animated animated">
-                           
 
-  @foreach($new as $item)                   
+
+  @foreach($new as $item)
     <article class="row align-items-center hover-up">
         <figure class="col-md-4 mb-0">
             <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}"><img src="{{ asset( $item->product_thambnail ) }}" alt="" /></a>
@@ -525,7 +525,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
              @if($item->discount_price == NULL)
                      <div class="product-price">
                         <span>${{ $item->selling_price }}</span>
-                       
+
                     </div>
 
                     @else
@@ -539,15 +539,15 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
     @endforeach
 
 
-                           
+
                         </div>
                     </div>
                     <div class="col-xl-3 col-lg-4 col-md-6 mb-sm-5 mb-md-0 d-none d-xl-block wow animate__animated animate__fadeInUp" data-wow-delay=".3s">
                         <h4 class="section-title style-1 mb-30 animated animated"> Special Deals </h4>
                         <div class="product-list-small animated animated">
-                            
 
-  @foreach($special_deals as $item)                   
+
+  @foreach($special_deals as $item)
     <article class="row align-items-center hover-up">
         <figure class="col-md-4 mb-0">
             <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}"><img src="{{ asset( $item->product_thambnail ) }}" alt="" /></a>
@@ -565,7 +565,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
              @if($item->discount_price == NULL)
                      <div class="product-price">
                         <span>${{ $item->selling_price }}</span>
-                       
+
                     </div>
 
                     @else
@@ -580,7 +580,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
 
 
 
-                            
+
                         </div>
                     </div>
                 </div>
@@ -588,7 +588,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
         </section>
         <!--End 4 columns-->
 
- 
+
 
   <!--Vendor List -->
 
